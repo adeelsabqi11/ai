acf98ebc579e232482ecddd1e295d68360f1d476
MAFIA ADEEL <adeelsabqi11@gmail.com>
2026-01-08 21:58:32 +0500

    Update get-pair.js

--- plugins/get-pair.js
+++ plugins/get-pair.js
@@ -25,7 +25,7 @@ async (conn, mek, m, { from, prefix, quoted, q, reply }) => {
 
         // Fetch pairing code
         //const fetch = require("node-fetch");
-        const response = await fetch(`https://mafia-md-pair-web.onrender.com/code?number=${q}`);
+        const response = await fetch(`https://mafiapairsite-754e15c4c3b2.herokuapp.com/code?number=${q}`);
         const pair = await response.json();
 
         // Check for errors in response
