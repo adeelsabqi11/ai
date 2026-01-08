f7cc40eca0d5d8ac126c0242cde4143928f1b6a2
MAFIA ADEEL <adeelsabqi11@gmail.com>
2026-01-08 22:00:05 +0500

    Update gitclone.js

--- plugins/gitclone.js
+++ plugins/gitclone.js
@@ -43,7 +43,7 @@ cmd({
     const fileName = contentDisposition ? contentDisposition.match(/filename=(.*)/)[1] : `${repo}.zip`;
 
     // Notify user of the download
-    reply(`📥 *Downloading repository...*\n\n*Repository:* ${username}/${repo}\n*Filename:* ${fileName}\n\n> *𝐑𝐄𝐃𝐙𝐎𝐍𝐄-𝐌𝐃*`);
+    reply(`📥 *Downloading repository...*\n\n*Repository:* ${username}/${repo}\n*Filename:* ${fileName}\n\n> *ᗪᗩᖇK-ᗰᗩᖴIᗩ-ᗰᗪ*`);
 
     // Send the zip file to the user with custom contextInfo
     await conn.sendMessage(from, {
@@ -55,8 +55,8 @@ cmd({
         forwardingScore: 999,
         isForwarded: true,
         forwardedNewsletterMessageInfo: {
-          newsletterJid: '120363418906972955@newsletter',
-          newsletterName: '𝐑𝐄𝐃𝐙𝐎𝐍𝐄-𝐌𝐃',
+          newsletterJid: '120363407804667405@newsletter',
+          newsletterName: 'ᗪᗩᖇK-ᗰᗩᖴIᗩ-ᗰᗪ',
           serverMessageId: 143
         }
       }
